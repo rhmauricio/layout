ce915a1401360b6df5e46cb0da64800ae5553253
rhmauricio <mauricio.ramirez1@udea.edu.co>
2017-09-03 01:24:20 -0500

    Initial commit

---
+++ app/src/main/java/rhmauricio/layout/layoutMainActivity.java
@@ -0,0 +1,13 @@
+package rhmauricio.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class layoutMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_main);
+    }
+}
